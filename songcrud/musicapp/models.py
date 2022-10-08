18e9928e44abcf359b186756af1b1bd9abbe2a67
0lanr3w4ju <oa211048@gmail.com>
2022-10-08 14:15:00 +0100

    added lyrics song and artist model

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.ImageField
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    date_released = models.DateField
+    likes = models.ImageField
+    artiste_id = models.ForeignKey(Artist,on_delete=models.CASCADE)
+
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song,on_delete=models.CASCADE)
